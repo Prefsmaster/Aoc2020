48ebb362f289c64e48b9103c8d01f7856d449f51
Ferenc Németh <ferencn@gmail.com>
2020-12-09 11:08:35 +0100

    Compacted a bit more

--- Day9/AocDay9.cs
+++ Day9/AocDay9.cs
@@ -9,45 +9,37 @@ namespace Day9
         private static void Main()
         {
             var values = File.ReadAllLines("input.txt").Select(long.Parse).ToArray();
-
+            // Part 1
             var invalidNumber = InvalidNumber(25, values);
             Console.WriteLine(invalidNumber);
+            // Part 2
             Console.WriteLine(FindWeakness(invalidNumber,values));
         }
 
         private static long InvalidNumber(int preamble, long[] numbers)
         {
-
             for (var candidateIndex = preamble; candidateIndex < numbers.Length; candidateIndex++)
             {
                 var found = false;
                 var candidate = numbers[candidateIndex];
                 for (var i1= candidateIndex-preamble; i1<candidateIndex && !found; i1++)
-                {
-                    for (var i2 = i1 + 1; i2<candidateIndex && !found; i2++)
-                    {
-                        found = (numbers[i1] + numbers[i2] == candidate);
-                    }
-                }
+                    for (var i2 = i1 + 1; i2 < candidateIndex && !(found = numbers[i1] + numbers[i2] == candidate); i2++) {}
                 if (!found) return candidate;
             }
-            return -1; // Indicate failure
+            return -1; // Indicates all numbers are valid
         }
 
+        // use sliding window approach. 
+        // add from end until target reached or too big
+        // subtract from start until target reached or too small
         private static long FindWeakness(long targetValue, long[] numbers)
         {
-            var startIndex = 0;
-            var endIndex = 0;
+            int startIndex = 0, endIndex = 0;
             var sum = 0L;
 
             while (sum != targetValue || endIndex-startIndex <=2)
             {
-                // use sliding window approach
-
-                // add from end until target reached or too big
                 while (sum < targetValue) sum += numbers[endIndex++];
-
-                // subtract from start until target reached or too small
                 while (sum > targetValue) sum -= numbers[startIndex++];
             }
 
